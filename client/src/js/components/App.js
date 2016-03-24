27bc3a3346f6c8a443c6a73b4c85406dd8ebbd20
Steve Lee <steve@opendirective.com>
2016-03-24 17:08:02 +0000

    fix lint warnings

--- client/src/js/components/App.js
+++ client/src/js/components/App.js
@@ -1,15 +1,13 @@
 import {Observable} from 'rx'
 import {section, div, button, img, p} from '@cycle/dom'
 
-
 function model() {
   return Observable.just({
-                          one: {image: '/img/montie.jpg', label: 'Montie at rest'},
-                          two: {image: '/img/032.jpg', label: 'Totem Pole'},
-                          three: {image: 'https://i.ytimg.com/vi/hSU-PvQBx0A/maxresdefault.jpg', label: 'Pimp my pooch'},
-                          four: {image: 'https://i.ytimg.com/vi/19nm5_nAwQg/hqdefault.jpg', label: 'Native Indian Musician'}
-  }
-  )
+    one: {image: '/img/montie.jpg', label: 'Montie at rest'},
+    two: {image: '/img/032.jpg', label: 'Totem Pole'},
+    three: {image: 'https://i.ytimg.com/vi/hSU-PvQBx0A/maxresdefault.jpg', label: 'Pimp my pooch'},
+    four: {image: 'https://i.ytimg.com/vi/19nm5_nAwQg/hqdefault.jpg', label: 'Native Indian Musician'}
+  })
 }
 
 function view(state$) {
