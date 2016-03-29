cd54bafa6613ff6812afc75e9ef5980e08574d26
Steve Lee <steve@opendirective.com>
2016-03-29 13:15:31 +0100

    Add HTTP driver

--- client/src/js/main.js
+++ client/src/js/main.js
@@ -1,10 +1,12 @@
 import {run} from '@cycle/core'
 import {makeDOMDriver} from '@cycle/dom'
+import {makeHTTPDriver} from '@cycle/http';
 import runHot from './runHot'
 import App from './components/App'
 
 const drivers = {
-  DOM: makeDOMDriver('#root')
+  DOM: makeDOMDriver('#root'),
+  HTTP: makeHTTPDriver()
 }
 
 if (module.hot) {   // hot loading enabled in config
