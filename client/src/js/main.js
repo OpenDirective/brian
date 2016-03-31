581a42aa49719ce201521ef851c1cc5e8b85fdf4
Steve Lee <steve@opendirective.com>
2016-03-31 19:53:08 +0100

    Change to correct history module

--- client/src/js/main.js
+++ client/src/js/main.js
@@ -1,13 +1,18 @@
 import {run} from '@cycle/core'
 import {makeDOMDriver} from '@cycle/dom'
 import {makeHTTPDriver} from '@cycle/http'
-import {makeHistoryDriver} from 'cycle-history'
+import {makeHistoryDriver, supportsHistory} from '@cycle/history'
+import {useQueries, createHistory, createHashHistory} from 'history'
 import runHot from './runHot'
 import App from './components/App'
 
+const history = supportsHistory() ?
+  createHistory() : createHashHistory()
+
 const drivers = {
   DOM: makeDOMDriver('#root'),
-  HTTP: makeHTTPDriver()
+  HTTP: makeHTTPDriver(),
+  history: makeHistoryDriver(history, {capture: false})
 }
 
 if (module.hot) {   // hot loading enabled in config
