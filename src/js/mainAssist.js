f7e7c8ab22e34eac46c197d51969afda0922b286
Steve Lee <steve@opendirective.com>
2016-05-04 07:53:54 +0100

    Remove unused aws deps

--- src/js/mainAssist.js
+++ src/js/mainAssist.js
@@ -5,16 +5,6 @@ import addGlobalErrorHandler from './utilities/globalError'
 import runHot from './utilities/runHot'
 import AssistApp from './components/AssistApp'
 
-/*
-require("script!./vendor/jsbn.js")
-require("script!./vendor/jsbn2.js")
-require("script!./vendor/sjcl.js")
-require("script!./vendor/moment.min.js")
-require("script!./vendor/aws-cognito-sdk.min.js")
-require("script!./vendor/amazon-cognito-identity.min.js")
-//require("script!../aws-sdk-2.3.5.js")
-*/
-
 const PRODUCTION = (process.env.NODE_ENV === 'production')
 
 // note while this is usefull it breaks the source mapping in console error messages
