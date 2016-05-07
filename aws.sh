eeb9aefa258e815b527876e2642871383fcc7191
Steve Lee <steve@opendirective.com>
2016-05-07 10:57:22 +0100

    Improve aws script

--- aws.sh
+++ aws.sh
@@ -1,21 +1,24 @@
-# TODO
-# check region - default should be eu
+# Create the Identity Pool - linked to our User Pool
 
-# lose quotes in command outputs
-export JP_UNQUOTED=true
+# TODO
+# Check region is correct or always specify it - default should be eu
+# Create User Pool and IAM roles. Or wait for serverless to support User Pools :)
+# Delete and recreat needed for initial dev and stage workflow
 
 export IDENTITY_POOL_NAME="BrianDARTTrial_Stage2"
 export USER_POOL_ID="us-east-1_A2HeABQfl"
+export USER_POOL_PREFIX="cognito-idp.us-east-1.amazonaws.com/"
 export APP_CLIENT_ID="7m5nr6pgpe7en26q55jnic16t6"
 
 IDENTITY_POOL_ID=`AWS cognito-identity create-identity-pool \
 --identity-pool-name $IDENTITY_POOL_NAME \
 --no-allow-unauthenticated-identities \
---cognito-identity-providers ProviderName=cognito-idp.us-east-1.amazonaws.com/$USER_POOL_ID,ClientId=$APP_CLIENT_ID \
+--cognito-identity-providers ProviderName=$USER_POOL_PREFIX$USER_POOL_ID,ClientId=$APP_CLIENT_ID \
 --query IdentityPoolId \
+--output text
 `
-IDENTITY_POOL_ID="${IDENTITY_POOL_ID%\"}";IDENTITY_POOL_ID="${IDENTITY_POOL_ID#\"}"
-echo "IDENTITY_POOL_ID: $IDENTITY_POOL_ID"
+
+echo "Identity Pool Id: $IDENTITY_POOL_ID"
 
 POOL_AUTH_ROLE="arn:aws:iam::063024541135:role/Cognito_BrianDARTTrial_StageAuth_Role"
 POOL_UNAUTH_ROLE="arn:aws:iam::063024541135:role/Cognito_BrianDARTTrial_StageUnauth_Role"
