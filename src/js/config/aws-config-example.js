3b330687fd22bdf5cb183c93702385e721ab15a4
Steve Lee <steve@opendirective.com>
2016-05-11 20:23:09 +0100

    Add AWSConfig example

---
+++ src/js/config/aws-config-example.js
@@ -0,0 +1,11 @@
+// AWS Config - is git ignored for privacy
+
+const AWSCONFIG = {
+  region: 'eu-west-1',
+  identityPoolId: 'eu-west-1:23f3754f-d6d3-47da-af70-5eb83441d510',
+  userPoolRegion: 'us-east-1',
+  userPoolId: 'us-east-1_A2HeABQfl',
+  clientId: '7m5nr6pgpe7en26q55jnic16t6'
+}
+
+export default AWSCONFIG
