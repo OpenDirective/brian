a4ceb0c5934e280fed4a3ba149d1e9d2bb1b647e
Steve Lee <steve@opendirective.com>
2016-05-13 22:52:49 +0100

    Tidy up AWS config

--- src/js/config/aws-config-example.js
+++ src/js/config/aws-config-example.js
@@ -1,11 +1,13 @@
-// AWS Config - is git ignored for privacy
+// Rename to aws-config and populat
 
 const AWSCONFIG = {
-  region: 'eu-west-1',
-  identityPoolId: 'eu-west-1:23f3754f-d6d3-47da-af70-5eb83441d510',
-  userPoolRegion: 'us-east-1',
-  userPoolId: 'us-east-1_A2HeABQfl',
-  clientId: '7m5nr6pgpe7en26q55jnic16t6'
+  // Note Amazon's Pascal casing of names
+  IdentityRegion: 'eu-west-1',
+  IdentityPoolId: 'eu-west-1:23f3754f-d6d3-47da-af70-5eb83441d510',
+
+  UserPoolRegion: 'us-east-1',
+  UserPoolId: 'us-east-1_A2HeABQfl',
+  ClientId: '7m5nr6pgpe7en26q55jnic16t6'
 }
 
 export default AWSCONFIG
