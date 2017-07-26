bb20fcd859c37194329c96fa93e7ae0af8f08a16
Steve Lee <steve@opendirective.com>
2017-07-26 11:33:32 +0100

    A few config tweaks

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,14 @@
-const path = require('path');
+const path = require('path')
 
-const appPath = (...names) => path.join(process.cwd(), ...names);
+const appPath = (...names) => path.join(process.cwd(), ...names)
 
 //This will be merged with the config from the flavor
 module.exports = {
     entry: {
-        main: [
-            appPath('src', 'index.ts'),
-            appPath('src', 'css', 'styles.scss')
-        ]
+        main: [appPath('src', 'index.ts'), appPath('src', 'css', 'styles.scss')]
     },
     output: {
         filename: 'bundle.[hash].js',
         path: appPath('build')
     }
-};
+}
