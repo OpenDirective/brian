0a423a85d9cc41ff3ee248dd730abe752ae00539
Steve Lee <steve@opendirective.com>
2017-08-04 17:42:30 +0100

    Update Speaker to be like the one-fit-all flavor

--- src/components/speaker.tsx
+++ src/components/speaker.tsx
@@ -1,4 +1,5 @@
 import xs, { Stream } from 'xstream'
+import sampleCombine from 'xstream/extra/sampleCombine'
 import { VNode, DOMSource } from '@cycle/dom'
 import { StateSource } from 'cycle-onionify'
 import { RouterSource, RouteMatcher } from 'cyclic-router'
@@ -20,44 +21,77 @@ export interface State {
 const defaultState: State = { text: 'Edit me!' }
 export type Reducer = (prev?: State) => State | undefined
 
+// Actions
+const SPEECH = 'speech',
+    NAVIGATE = 'navigate',
+    UPDATE = 'update'
+interface SpeechAction {
+    type: typeof SPEECH
+}
+interface NavigationAction {
+    type: typeof NAVIGATE
+}
+interface UpdateAction {
+    type: typeof UPDATE
+    reducer: Reducer
+}
+type Action = SpeechAction | NavigationAction | UpdateAction
+
 export function Speaker({ DOM, onion }: Sources): Sinks {
-    const action$: Stream<Reducer> = intent(DOM)
-    const vdom$: Stream<VNode> = view(onion.state$)
-
-    const touchSpeech$ = DOM.select('[data-action="speak"]')
-        .events('click')
-        .map(({ currentTarget }) => (currentTarget as Element).textContent)
-        .map(text => (typeof text === 'string' ? text : ''))
-
-    const routes$ = DOM.select('[data-action="navigate"]')
-        .events('click')
-        .mapTo('/')
-
-    const logout$ = DOM.select('[data-action="logout"]')
-        .events('click')
-        .mapTo({ action: 'logout' })
+    const action$: Stream<Action> = intent(DOM)
 
     return {
-        DOM: vdom$,
-        speech: touchSpeech$,
-        onion: action$,
-        router: routes$,
-        auth0: logout$
+        DOM: view(onion.state$),
+        speech: speech(action$, onion.state$),
+        onion: onionFn(action$),
+        router: router(action$)
     }
 }
 
-function intent(DOM: DOMSource): Stream<Reducer> {
-    //const init$: Stream<Reducer> = xs.of<Reducer>(p => ({ count: 10 }))
+function router(action$: Stream<Action>): Stream<string> {
+    return action$.filter(({ type }) => type === NAVIGATE).mapTo('/')
+}
+
+function speech(
+    action$: Stream<Action>,
+    state$: Stream<State>
+): Stream<string> {
+    return action$
+        .filter(({ type }) => type === SPEECH)
+        .compose(sampleCombine(state$))
+        .map<string>(([_, s]) => s.text)
+}
+
+function intent(DOM: DOMSource): Stream<Action> {
+    const updateText$: Stream<Action> = DOM.select('#text')
+        .events('input')
+        .map((ev: any) => ev.target.value)
+        .map<Action>((value: string) => ({
+            type: UPDATE,
+            reducer: () => ({ text: value })
+        }))
+
+    const speech$: Stream<Action> = DOM.select('[data-action="speak"]')
+        .events('click')
+        .mapTo<Action>({ type: SPEECH })
+
+    const navigation$: Stream<Action> = DOM.select('[data-action="navigate"]')
+        .events('click')
+        .mapTo<Action>({ type: NAVIGATE })
+
+    return xs.merge(updateText$, speech$, navigation$)
+}
+
+function onionFn(action$: Stream<Action>): Stream<Reducer> {
     const init$ = xs.of<Reducer>(
         prevState => (prevState === undefined ? defaultState : prevState)
     )
 
-    const textValue$: Stream<Reducer> = DOM.select('#text')
-        .events('input')
-        .map((ev: any) => ev.target.value)
-        .map<Reducer>(value => () => ({ text: value }))
+    const update$: Stream<Reducer> = action$
+        .filter(({ type }) => type === UPDATE)
+        .map<Reducer>((action: UpdateAction) => action.reducer)
 
-    return xs.merge(init$, textValue$)
+    return xs.merge(init$, update$)
 }
 
 function view(state$: Stream<State>): Stream<VNode> {
