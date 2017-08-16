1e2ef98f19ae8fd3b9e063a8591cb4b00fe56427
Steve Lee <steve@opendirective.com>
2017-08-16 16:13:26 +0100

    move secrest in apps settings

--- functionApp/src/getGoogleAlbums/index.js
+++ functionApp/src/getGoogleAlbums/index.js
@@ -1,34 +1,13 @@
-// constants - should be in app settings not here
-const AUTH0_DOMAIN_URL = 'https://odbrian.eu.auth0.com'
-const AUTH0_API_ID = 'https://brianAPI'
-const AUTH0_SIGNING_CERTIFICATE = `-----BEGIN CERTIFICATE-----
-MIIDAzCCAeugAwIBAgIJMmdlKJbhL/OQMA0GCSqGSIb3DQEBCwUAMB8xHTAbBgNV
-BAMTFG9kYnJpYW4uZXUuYXV0aDAuY29tMB4XDTE3MDczMTE5NTM0N1oXDTMxMDQw
-OTE5NTM0N1owHzEdMBsGA1UEAxMUb2Ricmlhbi5ldS5hdXRoMC5jb20wggEiMA0G
-CSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQC6mEyHQeLP/nUk74fx0Ztz6acgSrEi
-YVGsItJjv8jthjNRxhoO5PlIgwvZjXGgGr2OBiTeFmNMVHB+gtWLff+wNTXLJgKs
-tKK7lEUOVQvcNFdvVlB60QZlFpa2D8pyrvJ8jQvc1hbc7Rt0G7+4je/BpkZe8m6D
-d8Lt3pRBKI1N3HJWuepBzU7TTol0zmZbpANSnsQqkPkolrHfPXgxCjZnhyyac/QR
-0ggvLz0dwOId6b2aQ0TFNQ8SRpRGRCabHoKRzzalSUrAT5sFIu6NWocGXs3ykx25
-CHCKt0pLvIGIlDSxlp2e1jRIK3YT/Df2ZbALSz0UQIwJy4B+wqkBT76RAgMBAAGj
-QjBAMA8GA1UdEwEB/wQFMAMBAf8wHQYDVR0OBBYEFB8jHZ2bIJk9BV1AVecTMOje
-3O2NMA4GA1UdDwEB/wQEAwIChDANBgkqhkiG9w0BAQsFAAOCAQEAqK2UUWWvjGLx
-A2dNWrmlPKcBhsLvR/b1chteEMpKsbIPX56cRLxeU4O1ecjH0UceSGyV+gEOWGkl
-tRfyjEPafiuActlSZU3E1d9/+EuAXqyrT0jp02gzR+WpUCdxe+hKv6o2AcOP5aY2
-oNhZo7zn0RZhyeEYttuGG4bcnFLUWHLmJcxvdCiW2sO4FA+w6O2/OAl9Mj4d2M4t
-55KhujAfqeAhAHt3TI+tHXgNVgrrx9YPrLKELyJbbllkvj8q3U7aY0u6ECh36ook
-Zm79V3A1GVXg333qQBZ19gSFNzSMJ2Mad2TlBPjfTV7EpusdAF5TeNWQGOWL90px
-Rx/U48BoDA==
------END CERTIFICATE-----`
-const AUTH0_ADMIN_CLIENT_ID = 'ipW0LTc7RcCatxlT1qmKhdmRqvHpmiNP'
-const AUTH0_ADMIN_CLIENT_SECRET =
-    'a-ohe97u3zxikvKhYFbksspoWFq1MX4_-94Am7kg8l2nQPr30GYvDzLnG7EN-RwD'
-//const AZURE_APPINSIGHTS_KEY = 'bec7a79e-092e-4cb1-9217-b84cc23ff838'
-
-//var appInsights = require('applicationinsights')
-//var client = appInsights.getClient(AZURE_APPINSIGHTS_KEY)
-
 const request = require('request')
+
+const AUTH0_DOMAIN_URL = process.env.AUTH0_DOMAIN_URL
+const AUTH0_API_ID = process.env.AUTH0_API_ID
+const AUTH0_API_SIGNING_CERTIFICATE = process.env.AUTH0_API_SIGNING_CERTIFICATE
+const AUTH0_ADMIN_CLIENT_ID = process.env.AUTH0_ADMIN_CLIENT_ID
+const AUTH0_ADMIN_CLIENT_SECRET = process.env.AUTH0_ADMIN_CLIENT_SECRET
+
+console.log ("zzzzzzz",AUTH0_DOMAIN_URL)
+
 
 // Call a remote HTTP endpoint and return a JSON object
 function requestObject(options) {
@@ -74,7 +53,8 @@ function getUserProfile(accessToken, userID) {
         method: 'GET',
         url: `${AUTH0_DOMAIN_URL}/api/v2/users/${userID}`,
         headers: {
-            Authorization: `Bearer ${accessToken}`
+            Authorization: `Bearer ${accessToken}`,
+            'content-type': 'application/json'
         }
     }
     return requestObject(options)
@@ -96,22 +76,13 @@ function getAlbums(accessToken) {
 // Create decorator that checks the JWT signature and specified fields
 const jwtValidateDecorator = require('./azure-functions-auth0')({
     clientId: AUTH0_API_ID,
-    clientSecret: AUTH0_SIGNING_CERTIFICATE,
+    clientSecret: AUTH0_API_SIGNING_CERTIFICATE,
     algorithms: ['RS256'],
     domain: `${AUTH0_DOMAIN_URL}/`
 })
 
 // The main Functions Function
 module.exports = jwtValidateDecorator((context, req) => {
-    client.trackEvent('custom event', {
-        customProperty: 'custom property value'
-    })
-    client.trackException(
-        new Error('handled exceptions can be logged with this method')
-    )
-    client.trackMetric('custom metric', 3)
-    client.trackTrace('trace message')
-    var start = new Date()
     if (req.user) {
         // Get a token to access the admin API
         context.log('Token Validated')
