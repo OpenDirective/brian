645a38a83fa460c6e9afece739faa83aa2d16fbe
Steve Lee <steve@opendirective.com>
2017-08-18 15:37:18 +0100

    Allow API_HOST to be defiend in client env

--- src/environment.ts
+++ src/environment.ts
@@ -1,7 +1,11 @@
 // made availabe by webpack plugins
 // Unused VARS in .env are NOT included in the bundle
-const ISPRODUCTION = process.env.NODE_ENV === 'production'
+export const ISPRODUCTION = process.env.NODE_ENV === 'production'
 
-export const API_HOST = ISPRODUCTION
-    ? process.env.PRODUCTION_HOST
-    : process.env.DEVELOPMENT_HOST
+if (!(process.env.PRODUCTION_HOST && process.env.DEVELOPMENT_HOST)) {
+    throw 'PRODUCTION_HOST and DEVELOPMENT_HOST must be defined in env'
+}
+
+export const API_HOST = process.env.API_HOST
+    ? process.env.API_HOST as string
+    : ISPRODUCTION ? process.env.PRODUCTION_HOST : process.env.DEVELOPMENT_HOST
