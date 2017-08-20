d1e1530895081fd4c71acb2525708ccb16396b0c
Steve Lee <steve@opendirective.com>
2017-08-20 20:56:43 +0100

    Add getAlbumContents

--- functionApp/src/getPhotoAlbums/index.ts
+++ functionApp/src/getPhotoAlbums/index.ts
@@ -1,15 +1,19 @@
 import { auth0ifyBrianAPI } from '../providers//auth0'
-import { getPhotoAlbumList } from '../providers/google'
+import { getPhotoAlbumList, getPhotoAlbumContents } from '../providers/google'
 
 export = auth0ifyBrianAPI(['photos'], async (context, req) => {
     try {
         const userId = req.user.sub
-        const titles = await getPhotoAlbumList(userId)
-
-        const titlesText = JSON.stringify(titles)
+        const albums = await getPhotoAlbumList(userId, context)
+        const photos = await getPhotoAlbumContents(
+            userId,
+            albums[0].id,
+            context
+        )
+        const test = JSON.stringify(photos, undefined, 4)
         context.done(null, {
             status: 200,
-            body: titlesText,
+            body: test,
             headers: { 'Content-Type': 'application/json' }
         })
     } catch (err) {
--- functionApp/src/providers/google.ts
+++ functionApp/src/providers/google.ts
@@ -1,23 +1,76 @@
 import { requestObject, getProviderAccessToken } from './auth0'
 
-interface PhotoAlbumTitle {
+interface FeedEntry {
     title: { $t: string }
+    id: { $t: string }
+    gphoto$id: { $t: string }
 }
 
-interface PhotoTitlesFeed {
-    feed: { entry: PhotoAlbumTitle[] }
+interface FeedEntries {
+    feed: {
+        entry: FeedEntry[]
+    }
 }
 
-export async function getPhotoAlbumList(userId: string): Promise<string[]> {
+export interface AlbumEntry {
+    title: string
+    id: string
+}
+export interface PhotoEntry {
+    title: string
+    url: string
+}
+
+export async function getPhotoAlbumList(
+    userId: string,
+    context?: any
+): Promise<AlbumEntry[]> {
     const accessToken: string = await getProviderAccessToken(userId)
     const options = {
         method: 'GET',
-        //url: `https://www.googleapis.com/gmail/v1/users/me/labels`,
         url: 'https://picasaweb.google.com/data/feed/api/user/default?alt=json',
         headers: {
-            Authorization: `Bearer ${accessToken}`
+            Authorization: `Bearer ${accessToken}`,
+            'GData-Version': '3'
         }
     }
-    const { feed: { entry } } = await requestObject<PhotoTitlesFeed>(options)
-    return entry.map((ent: PhotoAlbumTitle) => ent.title.$t)
+    /*    context.log(
+        JSON.stringify(
+            (await requestObject<FeedEntries>(options)).feed.entry[0],
+            undefined,
+            4
+        )
+    )*/
+    const { feed: { entry } } = await requestObject<FeedEntries>(options)
+    return entry.map(
+        (ent: FeedEntry) =>
+            <AlbumEntry>{ title: ent.title.$t, id: ent.gphoto$id.$t }
+    )
+}
+
+export async function getPhotoAlbumContents(
+    userId: string,
+    albumId: string,
+    context?: any
+): Promise<PhotoEntry[]> {
+    const accessToken: string = await getProviderAccessToken(userId)
+    const options = {
+        method: 'GET',
+        url: `https://picasaweb.google.com/data/feed/api/user/default/albumid/${albumId}?alt=json`,
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+            'GData-Version': '3'
+        }
+    }
+    context.log(
+        JSON.stringify(
+            (await requestObject<FeedEntries>(options)).feed.entry[0],
+            undefined,
+            4
+        )
+    )
+    const { feed: { entry } } = await requestObject<FeedEntries>(options)
+    return entry.map(
+        (ent: FeedEntry) => <PhotoEntry>{ title: ent.title.$t, url: ent.id.$t }
+    )
 }
