ea7b5ae0d5a3774ee3bbafeecad278759d3fd146
Steve Lee <steve@opendirective.com>
2017-08-23 18:23:01 +0100

    Add grid layout to home screen

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -2,9 +2,28 @@ import xs, { MemoryStream, Stream } from 'xstream'
 import { VNode, DOMSource } from '@cycle/dom'
 import { ResponseStream, HTTPSource } from '@cycle/HTTP'
 import { StateSource } from 'cycle-onionify'
+import { Auth0Request, Logout } from 'cyclejs-auth0'
 
 import { BaseSources, BaseSinks } from '../interfaces'
 import { API_HOST } from '../environment'
+
+function speech(DOM: DOMSource): Stream<string> {
+    return DOM.select('[data-speech]').events('click').map(e => {
+        const he = e.target as HTMLElement
+        const value = he.dataset.speech as string
+        const text = he.textContent ? he.textContent : ''
+        const speech = value === '<text>' ? text : value
+        return speech
+    })
+}
+
+function routes(DOM: DOMSource): Stream<string> {
+    return DOM.select('[data-navigate="photos"]').events('click').map(e => {
+        const he = e.target as HTMLElement
+        const route = he.dataset.navigate as string
+        return `/${route}`
+    })
+}
 
 // Types
 export interface Sources extends BaseSources {
@@ -27,25 +46,10 @@ export type Reducer = (prev: State) => State | undefined
 
 const PHOTOS_URL = `${API_HOST}/api/getPhotoAlbums`
 
-export function Home({ HTTP, DOM, onion }: Sources): Sinks {
-    const action$: Stream<Reducer> = intent(HTTP)
+export function Home({ DOM, onion }: Sources): Sinks {
     const vdom$: Stream<VNode> = view(onion.state$)
 
-    const counterRoute$ = DOM.select(
-        '[data-action="navigate"][data-arg="counter"]'
-    )
-        .events('click')
-        .mapTo('/counter')
-    const speakerRoute$ = DOM.select(
-        '[data-action="navigate"][data-arg="speaker"]'
-    )
-        .events('click')
-        .mapTo('/speaker')
-    const routes$ = xs.merge(counterRoute$, speakerRoute$)
-
-    const photosAction$ = DOM.select('[data-action="fetch-photos"]').events(
-        'click'
-    )
+    /*
     const request$ = photosAction$.map(accessToken => ({
         url: PHOTOS_URL,
         method: 'get',
@@ -53,12 +57,18 @@ export function Home({ HTTP, DOM, onion }: Sources): Sinks {
         accept: 'application/json'
         // bearer header is added by auth0ify
     }))
+*/
+
+    const logout$ = DOM.select('[data-action="logout"]')
+        .events('click')
+        .mapTo({ action: 'logout' } as Logout)
 
     return {
         DOM: vdom$,
-        HTTP: request$,
-        onion: action$,
-        router: routes$
+        //     onion: action$,
+        router: routes(DOM),
+        auth0: logout$,
+        speech: speech(DOM)
     }
 }
 
@@ -69,7 +79,7 @@ function intent(HTTP: HTTPSource): Stream<Reducer> {
 
     const response$$ = HTTP.select('request-photos')
     const photos$ = response$$
-        /*        .map(response$ =>
+        .map(response$ =>
             response$.replaceError((error: any) => {
                 if (error.response) {
                     error.response.error.message =
@@ -79,7 +89,7 @@ function intent(HTTP: HTTPSource): Stream<Reducer> {
                     error.response ? error.response : { error, body: '' }
                 )
             })
-        )*/
+        )
         .flatten()
         .map<Reducer>(({ error, body }) => state => ({
             ...state,
@@ -95,19 +105,37 @@ function view(state$: Stream<State>): Stream<VNode> {
         .map(state => (state.error ? state.error : state.body))
         .map(result =>
             <div>
-                <h2>Easy access to Photos</h2>
-                <button type="button" data-action="navigate" data-arg="counter">
-                    Counter
-                </button>
-                <button type="button" data-action="navigate" data-arg="speaker">
-                    Speaker
-                </button>
-                <button type="button" data-action="fetch-photos">
-                    Get Photos
-                </button>
-                <pre className="result">
+                <div className="wrapper">
+                    <header className="header">
+                        <button
+                            type="button"
+                            className="header-title key"
+                            data-speech="<text>"
+                        >
+                            Touch the photo album you want to view &#x1f50a;
+                        </button>
+                        <button
+                            type="button"
+                            className="header-logout key"
+                            data-action="logout"
+                        >
+                            Logout
+                        </button>
+                    </header>
+
+                    <main className="grid">
+                        <button
+                            type="button"
+                            className="action key"
+                            data-navigate="photos"
+                        >
+                            View Photos
+                        </button>
+                    </main>
+                </div>
+                <footer className="result">
                     {JSON.stringify(result, undefined, 4)}
-                </pre>
+                </footer>
             </div>
         )
 }
